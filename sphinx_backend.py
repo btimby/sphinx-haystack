eb35ef7b7bb1571eb0264e19b183cf363794403f
Ben Timby <btimby@gmail.com>
2012-01-22 09:40:00 -0500

    Some hypothetical code

--- sphinx_backend.py
+++ sphinx_backend.py
@@ -2,35 +2,77 @@ import logging
 import warnings
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.db.models.loading import get_model
-from haystack.backends import BaseEngine, BaseSearchBackend, BaseSearchQuery, log_query, EmptyResults
-from haystack.constants import ID, DJANGO_CT, DJANGO_ID
-from haystack.exceptions import MissingDependency, MoreLikeThisError
-from haystack.inputs import PythonData, Clean, Exact
+from haystack.backends import BaseEngine, BaseSearchBackend, BaseSearchQuery
+from haystack.exceptions import MissingDependency
 from haystack.models import SearchResult
-from haystack.utils import get_identifier
 try:
-    # TODO: is it possible to use ORM instead?
     import MySQLdb
 except ImportError:
     raise MissingDependency("The 'sphinx' backend requires the installation of 'MySQLdb'. Please refer to the documentation.")
+try:
+    # Pool connections if SQLAlchemy is present.
+    import sqlalchemy.pool as pool
+    MySQLdb = pool.manage(MySQLdb)
+    connection_pooling = True
+except ImportError:
+    connection_pooling = False
+
+DEFAULT_PORT = 9306
 
 
 class SphinxSearchBackend(BaseSearchBackend):
+    # https://github.com/dcramer/django-sphinx/tree/master/djangosphinx/
     def __init__(self, connection_alias, **connection_options):
-        pass
+        # TODO: determine the version number of Sphinx.
+        # Parse from server banner "Server version: 1.10-dev (r2153)"
+        super(SphinxSearchBackend, self).__init__(connection_alias, **connection_options)
+        try:
+            self.conn_kwargs = {
+                'host': connection_options.get('HOST', 'localhost'),
+                'port': connection_options.get('PORT', DEFAULT_PORT),
+                'user': connection_options.get('USER'),
+                'passwd': connection_options.get('PASSWD'),
+            }
+        except KeyError, e:
+            raise ImproperlyConfigured('Missing connection parameter %s for sphinx-haystack.' % e.args[0])
+        try:
+            self.index_name = connection_options.get('INDEX')
+        except KeyError:
+            raise ImproperlyConfigured('Missing index name for sphinx-haystack. Please define INDEX.')
+        self.log = logging.getLogger('haystack')
+        if not connection_pooling:
+            self.log.WARNING('Connection pooling disabled. Install SQLAlchemy.')
+
+    def connect(self):
+        return MySQLdb.connect(*self.conn_kwargs)
 
     def update(self, index, iterable):
         """
         Issue an UPDATE query to Sphinx.
         """
-        raise NotImplementedError
+        values = []
+        # TODO determine fields.
+        fields = []
+        for item in iterable:
+            row = []
+            for field_name in fields:
+                row.append(getattr(item, field_name))
+            values.append(row)
+        conn = self.connect()
+        try:
+            conn.executemany('REPLACE INTO {0} ({1}) VALUES (%s, %s)'.format(self.index_name, ', '.join(fields)), values)
+        finally:
+            conn.close()
 
     def remove(self, obj_or_string):
         """
         Issue a DELETE query to Sphinx.
         """
-        raise NotImplementedError
+        conn = self.connect()
+        try:
+            conn.execute('DELETE FROM {0} WHERE id = %s'.format(self.index_name), (id, ))
+        finally:
+            conn.close()
 
     def clear(self, models=[], commit=True):
         raise NotImplementedError
@@ -41,7 +83,21 @@ class SphinxSearchBackend(BaseSearchBackend):
                narrow_queries=None, spelling_query=None, within=None,
                dwithin=None, distance_point=None,
                limit_to_registered_models=None, result_class=None, **kwargs):
-        raise NotImplementedError
+        if result_class is None:
+            result_class = SearchResult
+        conn = self.connect()
+        try:
+            curr = conn.cursor()
+            rows = curr.execute('SELECT * FROM {0} WHERE MATCH(%s)'.format(self.index_name), (query_string, ))
+        finally:
+            conn.close()
+        results = []
+        while True:
+            row = rows.fetchone()
+            if not row:
+                break
+            results.append(result_class(row))
+        return results
 
     def prep_value(self, value):
         return force_unicode(value)
