6b43ecfe96d9d2eff4a35d8031c82b3ec32bf334
Ben Timby <btimby@gmail.com>
2012-01-23 18:03:38 -0500

    update and delete now work (mostly).

--- sphinx_haystack.py
+++ sphinx_haystack.py
@@ -1,10 +1,15 @@
+import time
+import datetime
 import logging
 import warnings
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.encoding import force_unicode
 from haystack.backends import BaseEngine, BaseSearchBackend, BaseSearchQuery, log_query
 from haystack.exceptions import MissingDependency
 from haystack.models import SearchResult
+from haystack.utils import get_identifier
+from haystack.constants import ID, DJANGO_CT, DJANGO_ID
 try:
     import MySQLdb
 except ImportError:
@@ -39,7 +44,24 @@ class SphinxSearchBackend(BaseSearchBackend):
         if not connection_pooling:
             self.log.warning('Connection pooling disabled. Install SQLAlchemy.')
 
-    def connect(self):
+    def _from_python(self, value):
+        if isinstance(value, datetime.datetime):
+            value = time.mktime((value.year, value.month, value.day, value.hour, value.minute, value.second, 0, 0, 0))
+        elif isinstance(value, bool):
+            if value:
+                value = 1
+            else:
+                value = 0
+        elif isinstance(value, (list, tuple)):
+            value = u','.join([force_unicode(v) for v in value])
+        elif isinstance(value, (int, long, float)):
+            # Leave it alone.
+            pass
+        else:
+            value = force_unicode(value)
+        return value
+
+    def _connect(self):
         return MySQLdb.connect(**self.conn_kwargs)
 
     def update(self, index, iterable):
@@ -48,15 +70,25 @@ class SphinxSearchBackend(BaseSearchBackend):
         """
         values = []
         # TODO determine fields.
-        fields = []
+        fields, field_names = [], ['id']
+        for name, field in index.fields.items():
+            fields.append((name, field))
+            field_names.append(name)
         for item in iterable:
-            row = []
-            for field_name in fields:
-                row.append(getattr(item, field_name))
-            values.append(row)
-        conn = self.connect()
+            row = index.full_prepare(item)
+            row['id'] = item.id
+            values.append([row[f] for f in field_names])
+        conn = self._connect()
         try:
-            conn.executemany('REPLACE INTO {0} ({1}) VALUES (%s, %s)'.format(self.index_name, ', '.join(fields)), values)
+            curr = conn.cursor()
+            # TODO: is executemany() better than many execute()s?
+            curr.executemany('REPLACE INTO {0} ({1}) VALUES ({2})'.format(
+                self.index_name,
+                # Comma-separated list of field names
+                ', '.join(field_names),
+                # Comma-separated list of "%s", same number of them as field names.
+                ', '.join(('%s', ) * len(field_names))
+            ), values)
         finally:
             conn.close()
 
@@ -64,14 +96,19 @@ class SphinxSearchBackend(BaseSearchBackend):
         """
         Issue a DELETE query to Sphinx.
         """
-        conn = self.connect()
+        id = get_identifier(obj_or_string)
+        conn = self._connect()
         try:
-            conn.execute('DELETE FROM {0} WHERE id = %s'.format(self.index_name), (id, ))
+            curr = conn.cursor()
+            curr.execute('DELETE FROM {0} WHERE id = %s'.format(self.index_name), (id, ))
         finally:
             conn.close()
 
     def clear(self, models=[], commit=True):
-        raise NotImplementedError
+        # Do not issue a DELETE statement for the index. This will just add all
+        # the documents to the kill list. If the user actually wants to delete
+        # the index, they will need to issue an rm for the index data file and binlog.
+        raise NotImplementedError('Cannot delete index via Sphinx Backend.')
 
     @log_query
     def search(self, query_string, sort_by=None, start_offset=0, end_offset=None,
@@ -81,18 +118,28 @@ class SphinxSearchBackend(BaseSearchBackend):
                limit_to_registered_models=None, result_class=None, **kwargs):
         if result_class is None:
             result_class = SearchResult
-        conn = self.connect()
+        query = 'SELECT * FROM {0} WHERE MATCH(\'{1}\')'
+        if start_offset and end_offset:
+            query += ' LIMIT {0}, {1}'.format(start_offset, end_offset)
+        if end_offset:
+            query += ' LIMIT {0}'.format(end_offset)
+        conn = self._connect()
         try:
             curr = conn.cursor()
-            rows = curr.execute('SELECT * FROM {0} WHERE MATCH(\'{1}\')'.format(self.index_name, query_string))
+            rows = curr.execute(query.format(self.index_name, query_string))
         finally:
             conn.close()
         results = []
+        # TODO: determine these at run-time:
+        app_label = 'notes'
+        model_name = 'Note'
         while True:
             row = curr.fetchone()
             if not row:
                 break
-            results.append(result_class(row))
+            #app_label, model_name = row[DJANGO_CT].split('.')
+            id, score = row[:2]
+            results.append(result_class(app_label, model_name, id, score))
         hits = len(results)
         return {
             'results': results,
@@ -113,7 +160,22 @@ class SphinxSearchBackend(BaseSearchBackend):
 
 
 class SphinxSearchQuery(BaseSearchQuery):
+    def build_query(self):
+        # TODO: any fields that are not "full text" but an attribute in Sphinx, such
+        # as an int or timestamp column needs to be handled via regular WHERE clause syntax:
+        # ... WHERE attr = 1234 ...
+        # However "full text" fields need to be globbed together using the Sphinx Query syntax
+        # inside a MATCH() call:
+        # ... WHERE MATCH('@text keyword') ...
+        # Together this should yield something like:
+        # ... WHERE attr = 1234 AND MATCH('@text keyword') ...
+        return super(SphinxSearchQuery, self).build_query()
+
     def build_query_fragment(self, field, filter_type, value):
+        # TODO: this probably won't be needed in the future, the functionality here will
+        # be handled in build_query. However, since I am unsure how to determine what type
+        # of field each field is, we will just treat them all as "full text" fields and let
+        # the base implementation glob them for us.
         from haystack import connections
         if field == 'content':
             index_fieldname = '@* '
